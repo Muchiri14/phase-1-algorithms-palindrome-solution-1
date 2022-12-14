dd5a801ae6ab15e24e6aea6db0b008937a49b440
Daniel <muchiridaniel59@gmail.com>
2022-12-14 03:17:36 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 function isPalindrome(word) {
   // Write your algorithm here
+   // reverse the input string
+  function reverseString(word) {
+    return word.split("").reverse().join("");
+}
+const reversedWord = reverseString(word);
+// compare the reversed string to the input
+  return word === reversedWord
 }
 
-/* 
-  Add your pseudocode here
-*/
 
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
